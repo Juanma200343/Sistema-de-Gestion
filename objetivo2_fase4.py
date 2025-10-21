f144284dea3ccb7a3ba0fb231dd0a477f6025195
Juanma200343 <juanma200343@gmail.com>
2025-10-21 14:27:22 +0200

    Add files via upload

---
+++ objetivo2_fase4.py
@@ -0,0 +1,11 @@
+#Juan Manuel Domínguez García 2ºDAM
+
+#Ejercicio 1
+
+a = int(input("Dame el primer numero: ")) 
+b = int(input("Dame el segundo numero: ")) 
+c = int(input("Dame el tercer numero: ")) 
+
+print((a < b) and (b < c))
+print((a == b) and (b == c))
+print(not (a > c))
